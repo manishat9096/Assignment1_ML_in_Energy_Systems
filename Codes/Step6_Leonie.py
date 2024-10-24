8cbe0e83ee36d95f568d8ac97d8166add671a622
LeonieMeier <s233049@student.dtu.dk>
2024-10-24 21:54:49 +0200

    made the code nicer, ready for the test set (when test set is ready) some work is needed - not a lot)

--- Codes/Step6_Leonie.py
+++ Codes/Step6_Leonie.py
@@ -17,9 +17,101 @@ from Step_3_Final import y_pred_gd, y_pred_closed
 from Step_4_Final import y_pred_best #NON linear
 from Step_5_Final import y_pred_lasso,y_pred_ridge,y_pred_lasso_NLR,y_pred_ridge_NLR #NON linear
 
-# Import price data
-prices = pd.read_excel("C:/Users/leoni/Desktop/Machine Learning in Energy System/Assignment1_ML_in_Energy_Systems/Datasets/prices.xlsx")
-prices = prices.rename(
+
+# Choose the prediction model
+prediction_model = {"gradient_descent": y_pred_gd, 'closed_form': y_pred_closed, 'non_linear_model': y_pred_best
+                    , 'Linear_L1': y_pred_lasso, 'Linear_L2': y_pred_ridge, 'non_linear_L1': y_pred_lasso_NLR, 
+                    'non_linear_L2': y_pred_ridge_NLR}
+
+MLmodel = "non_linear_L2" # Could be 'closed_form' or 'gradient_descent',....
+
+
+farm_capacity = 6000  # kW
+
+
+def optimization_validation(prediction, TIME, prices, p_real):
+    
+    #just use the last 566 values
+    spot_price = prices['Spot price'].tail(len(prediction)).reset_index(drop=True)
+    UP_price = prices['Up reg price'].tail(len(prediction)).reset_index(drop=True)
+    DW_price = prices['Down reg price'].tail(len(prediction)).reset_index(drop=True)
+    
+    # Initialize optimization model
+    model = gb.Model("optimization_model")
+    model.Params.TimeLimit = 100
+    model.setParam("NonConvex", 2)
+    
+    # Define variables
+    bid = {t: model.addVar(lb=0, ub=farm_capacity, name=f"Wind power bid at time {t}") for t in TIME}
+    delta_plus = {t: model.addVar(lb=0, ub=gb.GRB.INFINITY, name=f"Positive difference at time {t}") for t in TIME}
+    delta_minus = {t: model.addVar(lb=0, ub=gb.GRB.INFINITY, name=f"Negative difference at time {t}") for t in TIME}
+    
+    # Define objective function
+    DA_revenue = gb.quicksum(spot_price[t] * bid[t] for t in TIME)
+    balancing_revenue = gb.quicksum(
+        DW_price[t] * delta_plus[t] - UP_price[t] * delta_minus[t] for t in TIME
+    )
+    model.setObjective(DA_revenue + balancing_revenue, GRB.MAXIMIZE)
+    
+    # Define constraints
+    for t in TIME:
+        model.addConstr(prediction[t] - bid[t] == delta_plus[t] - delta_minus[t], name=f"Prediction constraint at {t}")
+    
+    # Optimize the model
+    model.optimize()
+    
+    # Extract optimal bids
+    optimal_bid = {t: bid[t].x for t in TIME}
+    
+    optimal_objective = model.ObjVal
+    
+    # Plot results
+    plt.figure(figsize=(10, 6))
+    TIME1 = range(100, 300)
+    plt.plot(TIME1, [prediction[t] for t in TIME1], label="Prediction", color="blue", marker="o")
+    plt.plot(TIME1, [optimal_bid[t] for t in TIME1], label="Optimal Bid", color="green", marker="x")
+    plt.xlabel("Time (hours)")
+    plt.ylabel("Power (kW)")
+    plt.title("Optimal Bids vs Predictions Over Time")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    
+    # Calculate balance, DW, and UP
+    balance = {t: p_real[t] - optimal_bid[t] for t in TIME}
+    DW = {t: max(balance[t], 0) for t in TIME}  # Downward regulation
+    UP = {t: max(-balance[t], 0) for t in TIME}  # Upward regulation
+
+    # Calculate real DA and balancing revenue
+    DA_revenue = sum(spot_price[t] * optimal_bid[t] for t in TIME)
+    balancing_revenue = sum(
+        DW_price[t] * DW[t] - UP_price[t] * UP[t] for t in TIME
+    )
+
+    
+    return optimal_objective, DA_revenue+balancing_revenue
+
+################################# Validation ################################# 
+
+# Import actual power production (validation set)
+dataset = pd.read_csv("C:/Users/leoni/Desktop/Machine Learning in Energy System/Assignment1_ML_in_Energy_Systems/Datasets/Cumulative_dataset.csv")
+
+# Reverting normalization
+prediction1=prediction_model[MLmodel] 
+y_0 = -dataset['kalby_active_power']
+y_max = max(y_0)
+y_min = min(y_0)
+prediction = prediction1 * (y_max - y_min) + y_min
+
+TIME = range(len(prediction))  # Simpler range definition
+
+# Calculate actual revenue (real power production)
+p_real_val = -(dataset['kalby_active_power'].tail(len(prediction1)).reset_index(drop=True))
+
+
+# Import price data (validation set)
+prices_val_set = pd.read_excel("C:/Users/leoni/Desktop/Machine Learning in Energy System/Assignment1_ML_in_Energy_Systems/Datasets/prices.xlsx")
+prices_val_set = prices_val_set.rename(
     columns={
         "SpotPriceEUR": "Spot price",
         "BalancingPowerPriceUpEUR": "Up reg price",
@@ -27,96 +119,51 @@ prices = prices.rename(
     }
 )
 
-# Now divide the specified columns by 1000
+# Now divide the specified columns by 1000(in per kWh)
 columns_to_divide = ["Spot price", "Up reg price", "Down reg price"]
-prices[columns_to_divide] = prices[columns_to_divide] / 1000
-
-#just use the last 566 values
-spot_price = prices['Spot price'].tail(566).reset_index(drop=True)
-UP_price = prices['Up reg price'].tail(566).reset_index(drop=True)
-DW_price = prices['Down reg price'].tail(566).reset_index(drop=True)
-
-
-# Import actual power production
-dataset = pd.read_csv("C:/Users/leoni/Desktop/Machine Learning in Energy System/Assignment1_ML_in_Energy_Systems/Datasets/Cumulative_dataset.csv")
-
-# Parameters
-TIME = range(566)  # Simpler range definition
-
-# Choose the prediction model
-
-prediction_model = {"gradient_descent": y_pred_gd, 'closed_form': y_pred_closed, 'non_linear_model': y_pred_best
-                    , 'Linear_L1': y_pred_lasso, 'Linear_L2': y_pred_ridge, 'non_linear_L1': y_pred_lasso_NLR, 
-                    'non_linear_L2': y_pred_ridge_NLR}
-
-MLmodel = "non_linear_L2" # Could be 'closed_form' or 'gradient_descent'
-prediction1=prediction_model[MLmodel] 
-
-# Reverting normalization
-y_0 = -dataset['kalby_active_power']
-y_max = max(y_0)
-y_min = min(y_0)
-prediction = prediction1 * (y_max - y_min) + y_min
-
-farm_capacity = 6000  # kW
-
-# Initialize optimization model
-model = gb.Model("optimization_model")
-model.Params.TimeLimit = 100
-model.setParam("NonConvex", 2)
-
-# Define variables
-bid = {t: model.addVar(lb=0, ub=farm_capacity, name=f"Wind power bid at time {t}") for t in TIME}
-delta_plus = {t: model.addVar(lb=0, ub=gb.GRB.INFINITY, name=f"Positive difference at time {t}") for t in TIME}
-delta_minus = {t: model.addVar(lb=0, ub=gb.GRB.INFINITY, name=f"Negative difference at time {t}") for t in TIME}
-
-# Define objective function
-DA_revenue = gb.quicksum(spot_price[t] * bid[t] for t in TIME)
-balancing_revenue = gb.quicksum(
-    DW_price[t] * delta_plus[t] - UP_price[t] * delta_minus[t] for t in TIME
-)
-model.setObjective(DA_revenue + balancing_revenue, GRB.MAXIMIZE)
-
-# Define constraints
-for t in TIME:
-    model.addConstr(prediction[t] - bid[t] == delta_plus[t] - delta_minus[t], name=f"Prediction constraint at {t}")
-
-# Optimize the model
-model.optimize()
-
-# Extract optimal bids
-optimal_bid = {t: bid[t].x for t in TIME}
-
-# Plot results
-plt.figure(figsize=(10, 6))
-TIME1 = range(100, 300)
-plt.plot(TIME1, [prediction[t] for t in TIME1], label="Prediction", color="blue", marker="o")
-plt.plot(TIME1, [optimal_bid[t] for t in TIME1], label="Optimal Bid", color="green", marker="x")
-plt.xlabel("Time (hours)")
-plt.ylabel("Power (kW)")
-plt.title("Optimal Bids vs Predictions Over Time")
-plt.legend()
-plt.grid(True)
-plt.show()
-
-# Calculate actual revenue (real power production)
-p_real = -(dataset['kalby_active_power'].tail(566).reset_index(drop=True))
-
-# Calculate balance, DW, and UP
-balance = {t: p_real[t] - optimal_bid[t] for t in TIME}
-DW = {t: max(balance[t], 0) for t in TIME}  # Downward regulation
-UP = {t: max(-balance[t], 0) for t in TIME}  # Upward regulation
-
-# Calculate real DA and balancing revenue
-DA_revenue = sum(spot_price[t] * optimal_bid[t] for t in TIME)
-balancing_revenue = sum(
-    DW_price[t] * DW[t] - UP_price[t] * UP[t] for t in TIME
-)
+prices_val_set[columns_to_divide] = prices_val_set[columns_to_divide] / 1000
+  
+optimal_obj_val, real_revenue_val=optimization_validation(prediction,TIME, prices_val_set, p_real_val)
 
 # Print results
-print(f"Day-Ahead Revenue: {DA_revenue}")
-print(f"Balancing Revenue: {balancing_revenue}")
-print(f"Real Revenue ({MLmodel}): {DA_revenue + balancing_revenue}")
+print(f"Validation Set Optimal Objective: {optimal_obj_val}")
+print(f"Validation Set Real Revenue ({MLmodel}): {real_revenue_val}")
+
+
+################################### Test #################################### 
+# Import actual power production (test set)
+#
+#
+#
+
+# Import price data (test set)
+#
+#
+#
+#
+
+# Now divide the specified columns by 1000(in per kWh)
+#
+
+#optimal_obj_test, real_revenue_test=optimization_validation(prediction1,TIME, prices_test_set, p_real_test)
+
+# Print results
+#print(f"Test Set Optimal Objective: {optimal_obj_test}")
+#print(f"Test Set Real Revenue ({MLmodel}): {real_revenue_test}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ### Maja's code
